144f93427e5c1fa46f0dec4e9310f13fbddd27e3
saketh <123003115@sastra.ac.in>
2021-05-24 22:53:28 +0530

    validated regno

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#rcscarousel').carousel({interval:1200});
+    $('#rcscarousel').carousel({interval:1400});
     $('#rcscarousel').click(function(){
         
         $('#resultmodal').modal('show'); 
@@ -10,7 +10,7 @@ $(document).ready(function(){
     $('#checkr').click(function(){
         
         var spinArray = ['animation900','animation1080','animation1260','animation1440','animation1620','animation1800','animation1980','animation2160'];
-        var ar=[123003115,123003190,123003137,123003071];
+        var ar=[123003115,123003190,123003137,123003071,123004094];
     var fl=false;
     var regno=null
     regno=document.getElementById("reg").value;
@@ -21,23 +21,32 @@ $(document).ready(function(){
          }
     }
     function getSpin() {
+
         if(regno.length!=0){
-            if(fl){
-        $('#changeres').removeClass();
-        $('#changeres').addClass("invisible");
-        $('#coin-flip-cont').removeClass();
-        $('#coin-flip-cont').addClass("visible");
-        //document.getElementById("status").innerHTML="congratulations you are into the club"
-        var spin = spinArray[6];
-        }
-        else{
-        $('#changeres').removeClass();
-        $('#changeres').addClass("invisible");
-        $('#coin-flip-cont').removeClass();
-        $('#coin-flip-cont').addClass("visible");
-        //document.getElementById("status").innerHTML="sorry you are not into the club"
-        var spin = spinArray[7];
-        }
+            if(regno.length==9){
+                if(fl){
+                    $('#changeres').removeClass();
+                    $('#changeres').addClass("invisible");
+                    $('#coin-flip-cont').removeClass();
+                    $('#coin-flip-cont').addClass("visible");
+            //document.getElementById("status").innerHTML="congratulations you are into the club"
+                var spin = spinArray[6];
+                }
+                else{
+                    $('#changeres').removeClass();
+                    $('#changeres').addClass("invisible");
+                    $('#coin-flip-cont').removeClass();
+                    $('#coin-flip-cont').addClass("visible");
+            //document.getElementById("status").innerHTML="sorry you are not into the club"
+                var spin = spinArray[7];
+                }
+
+            }
+            else{
+                alert("Enter valid registration number");
+                
+            }
+            
         }
         
 return spin;
